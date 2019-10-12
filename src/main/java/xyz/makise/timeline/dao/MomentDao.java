37b59738aee58ec6d7fb6c34597fc1615d72b4e0
mikotokirino <shikirin556@gmail.com>
2019-10-12 12:45:24 +0800

    更改数据库设置为连接数据库 配置工作未完全完成

--- src/main/java/xyz/makise/timeline/dao/MomentDao.java
+++ src/main/java/xyz/makise/timeline/dao/MomentDao.java
@@ -13,43 +13,26 @@ public class MomentDao {
 
     //    这边不连接数据库 简单模拟一下数据库的操作
     public ArrayList<Moment> getMoments() {
-        Random random = new Random();
-        ArrayList<Moment> moments = new ArrayList<>();
-        for (int i = 0; i < momentOnce; i++) {
-            Moment moment = new Moment();
-            moment.setAuthor(getRandomString(6));
-            moment.setContent(getRandomString(random.nextInt(200)));
-            moment.setImageUrl(getRandomString(10) + ".pnc");
-            moment.setTimeStamp(getRandomTimeStamp());
-//            moment的lineCount由上一层进行注入
-            moments.add(moment);
-        }
-        return moments;
+        return null;
     }
 
     public void addMoment(){
-//        模拟的数据库把所有内容全部存到容器里的PageView对象中
-        Random random = new Random();
-        Moment moment = new Moment();
-        moment.setAuthor(getRandomString(6));
-        moment.setContent(getRandomString(random.nextInt(200)));
-        moment.setImageUrl(getRandomString(10) + ".pnc");
-        moment.setTimeStamp(Timestamp.valueOf(new Date().toString()));
+
     }
 
-    private String getRandomString(int length){
-        StringBuffer stringBuffer = new StringBuffer();
-        final String source = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        for (int i = 0; i < length; i++) {
-            stringBuffer.append(source.charAt(random.nextInt(source.length())));
-        }
-        return stringBuffer.toString();
-    }
-
-    private Timestamp getRandomTimeStamp(){
-        long offset = Timestamp.valueOf("2012-01-01 00:00:00").getTime();
-        long end = Timestamp.valueOf("2019-10-11 00:00:00").getTime();
-        long diff = end - offset + 1;
-        return new Timestamp(offset + (long)(Math.random() * diff));
-    }
+//    private String getRandomString(int length){
+//        StringBuffer stringBuffer = new StringBuffer();
+//        final String source = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//        for (int i = 0; i < length; i++) {
+//            stringBuffer.append(source.charAt(random.nextInt(source.length())));
+//        }
+//        return stringBuffer.toString();
+//    }
+//
+//    private Timestamp getRandomTimeStamp(){
+//        long offset = Timestamp.valueOf("2012-01-01 00:00:00").getTime();
+//        long end = Timestamp.valueOf("2019-10-11 00:00:00").getTime();
+//        long diff = end - offset + 1;
+//        return new Timestamp(offset + (long)(Math.random() * diff));
+//    }
 }
