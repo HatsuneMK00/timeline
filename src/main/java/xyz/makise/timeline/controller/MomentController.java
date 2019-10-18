203d35fe8f4f73ce1e8d3c4e6ef21d9155284b53
mikotokirino <shikirin556@gmail.com>
2019-10-19 00:14:41 +0800

    添加了接口说明文档框架swagger2

--- src/main/java/xyz/makise/timeline/controller/MomentController.java
+++ src/main/java/xyz/makise/timeline/controller/MomentController.java
@@ -1,5 +1,6 @@
 package xyz.makise.timeline.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import xyz.makise.timeline.entity.Moment;
 import xyz.makise.timeline.entity.PageView;
 import xyz.makise.timeline.service.AddMomentCommand;
@@ -19,13 +21,14 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.SplittableRandom;
 
-@Controller
+@RestController
 @ResponseBody
 @RequestMapping("/timeline")
 public class MomentController {
     @Autowired
     PageView pageView;
 
+    @ApiOperation(value="获取更多动态",notes = "每次获取三条新动态")
     @GetMapping("getmore")
     public PageView getMoreMoment(){
         Invoker invoker = new Invoker();
@@ -36,6 +39,7 @@ public class MomentController {
     }
 
 //    随机添加动态
+    @ApiOperation(value = "随机添加一条动态")
     @GetMapping("addmoment")
     public void addMoment(){
         Invoker invoker = new Invoker();
@@ -45,6 +49,7 @@ public class MomentController {
     }
 
 //    获取新动态
+    @ApiOperation(value = "刷新动态",notes = "先随机添加一条动态，再刷新动态重新获取最新的3条动态")
     @GetMapping("refresh")
     public PageView refreshMoment(){
         Invoker invoker = new Invoker();
